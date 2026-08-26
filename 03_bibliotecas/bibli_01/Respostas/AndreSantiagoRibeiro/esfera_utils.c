315b3d2b90fb6c5948afa16ac1a563223e27a6d1
Overwatch não presta <santiagoandre733@gmail.com>
2026-08-25 21:37:23 -0300

    DESGRACA

--- 03_bibliotecas/bibli_01/Respostas/AndreSantiagoRibeiro/esfera_utils.c
+++ 03_bibliotecas/bibli_01/Respostas/AndreSantiagoRibeiro/esfera_utils.c
@@ -1,9 +1,9 @@
 #include "esfera_utils.h"
 
 float calcula_volume (float R){
-    return 4/3 * PI * R*R*R;
+    return PI * (R*R*R) * 4 / 3;
 }
 
 float calcula_area (float R){
-    return 4 * PI * R*R;
+    return 4 * PI * (R*R);
 }
--- 03_bibliotecas/bibli_01/Resultados/AndreSantiagoRibeiro/completo/main.c
+++ 03_bibliotecas/bibli_01/Resultados/AndreSantiagoRibeiro/completo/main.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 int main(){
-    int n;
+    float n;
     scanf("%f", &n);
     printf("Area: %.2f\n", calcula_area(n));
     printf("Volume: %.2f\n", calcula_volume(n));
+
+    return 0;
 }
