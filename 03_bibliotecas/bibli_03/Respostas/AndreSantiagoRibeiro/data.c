40f1d46146c65bd265f2e463d2560b8efefa885b
asribeiro <santiagoandre733@gmail.com>
2026-08-27 12:59:30 -0300

    comeco da bibli_03

---
+++ 03_bibliotecas/bibli_03/Respostas/AndreSantiagoRibeiro/data.c
@@ -0,0 +1,105 @@
+#include "data.h"
+#include <stdio.h>
+
+int verificaDataValida(int dia, int mes, int ano){
+    if((mes < 1) || (mes > 12)){
+        return 0;
+    }
+    if((dia < 1) || (dia > numeroDiasMes(mes, ano))){
+        return 0;
+    }
+    return 1;
+}
+
+void imprimeMesExtenso(int mes){
+    if(mes == 1){
+        printf("Janeiro");
+    }
+    else if(mes == 2){
+        printf("Fevereiro");
+    }
+    else if(mes == 3){
+        printf("Marco");
+    }
+    else if(mes == 4){
+        printf("Abril");
+    }
+    else if(mes == 5){
+        printf("Maio");
+    }
+    else if(mes == 6){
+        printf("Junho");
+    }
+    else if(mes == 7){
+        printf("Julho");
+    }
+    else if(mes == 8){
+        printf("Agosto");
+    }
+    else if(mes == 9){
+        printf("Setembro");
+    }
+    else if(mes == 10){
+        printf("Outubro");
+    }
+    else if(mes == 11){
+        printf("Novembro");
+    }
+    else if(mes == 12){
+        printf("Dezembro");
+    }
+    return;
+}
+
+void imprimeDataExtenso(int dia, int mes, int ano){
+    printf("%d de ", dia);
+    imprimeMesExtenso(mes);
+    printf(" de %d", ano);
+    return;
+}
+
+int verificaBissexto(int ano){
+    if(ano%400 == 0){
+        return 1;
+    }
+    if(ano%100 == 0){
+        return 0;
+    }
+    if(ano%4 == 0){
+        return 1;
+    }
+    return 0;
+}
+
+int numeroDiasMes(int mes, int ano){
+    if((mes == 1) || (mes == 3) || (mes == 5) || (mes == 7) || (mes == 8) || (mes == 10) || (mes == 12)){
+        return 31;
+    }
+    if(mes == 2){
+        if(verificaBissexto(ano)){
+            return 29;
+        }
+        else {
+            return 28;
+        }
+    }
+    return 30;
+}
+
+int comparaData(int dia1, int mes1, int ano1, int dia2, int mes2, int ano2){
+
+}
+
+int calculaDiasAteMes(int mes, int ano){
+    int i, soma = 0;
+
+    for(i = 1; i < mes; i++){
+        soma += numeroDiasMes(i, ano);
+    }
+    return soma;
+
+}
+
+int calculaDiferencaDias(int dia1, int mes1, int ano1, int dia2, int mes2, int ano2){
+
+}
