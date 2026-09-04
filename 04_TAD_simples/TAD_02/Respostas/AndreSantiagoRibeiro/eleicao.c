95bc2abac61674f6f3b8bbddf0b76e9c16eb919d
asribeiro <santiagoandre733@gmail.com>
2026-09-04 12:49:38 -0300

    continuacao TAD_02

--- 04_TAD_simples/TAD_02/Respostas/AndreSantiagoRibeiro/eleicao.c
+++ 04_TAD_simples/TAD_02/Respostas/AndreSantiagoRibeiro/eleicao.c
@@ -22,4 +22,62 @@ tEleicao InicializaEleicao(){
             eleicao.totalGovernadores++;
         }
     }
+
+    eleicao.votosBrancosGovernador = 0;
+    eleicao.votosBrancosPresidente = 0;
+    eleicao.votosNulosGovernador = 0;
+    eleicao.votosNulosPresidente = 0;
+
+    return eleicao;
+}
+
+tEleicao RealizaEleicao(tEleicao eleicao){
+    int i;
+    scanf("%d ", &eleicao.totalEleitores);
+
+    for(i = 0; i < eleicao.totalEleitores; i++){
+        eleicao.eleitores[i] = LeEleitor();
+    }
+
+    return eleicao;
+}
+
+void ImprimeResultadoEleicao(tEleicao eleicao){
+    int i, j;
+
+    //contagem dos votos
+    for(i = 0; i < eleicao.totalEleitores; i++){
+        int verif = 0;
+        if(ObtemVotoGovernador(eleicao.eleitores[i]) == 0){
+            eleicao.votosBrancosGovernador++;
+        }
+        else {
+            for(j = 0; j < eleicao.totalGovernadores; j++){
+                if(VerificaIdCandidato(eleicao.governadores[j], ObtemVotoGovernador(eleicao.eleitores[i]))){
+                    IncrementaVotoCandidato(eleicao.governadores[j]);
+                    verif = 1;
+                }
+            }
+            if(!verif){
+                eleicao.votosNulosGovernador++;
+            }
+        }
+        verif = 0;
+        if(ObtemVotoPresidente(eleicao.eleitores[i]) == 0){
+            eleicao.votosBrancosPresidente++;
+        }
+        else {
+            for(j = 0; j < eleicao.totalPresidentes; j++){
+                if(VerificaIdCandidato(eleicao.presidentes[j], ObtemVotoPresidente(eleicao.eleitores[i]))){
+                    IncrementaVotoCandidato(eleicao.presidentes[j]);
+                    verif = 1;
+                }
+            }
+            if(!verif){
+                eleicao.votosNulosPresidente++;
+            }
+        }
+    }
+
+    
 }
