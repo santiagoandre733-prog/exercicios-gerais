96cc9a1ed51b8d320ae08401c1256cdd5b7550b8
asribeiro <santiagoandre733@gmail.com>
2026-09-21 12:40:27 -0300

    completo pont_07

--- 05_ponteiros/pont_07/Respostas/AndreSantiagoRibeiro/evento.c
+++ 05_ponteiros/pont_07/Respostas/AndreSantiagoRibeiro/evento.c
@@ -2,6 +2,10 @@
 #include "evento.h"
 
 void cadastrarEvento(Evento* eventos, int* numEventos){
+    if(*numEventos == MAX_EVENTOS){
+        printf("Limite de eventos atingido!\n");
+        return;
+    }
     scanf(" %[^\n]\n", eventos[*numEventos].nome);
     scanf("%d %d %d ", &eventos[*numEventos].dia, &eventos[*numEventos].mes, &eventos[*numEventos].ano);
     (*numEventos)++;
@@ -9,22 +13,36 @@ void cadastrarEvento(Evento* eventos, int* numEventos){
 }
 
 void exibirEventos(Evento* eventos, int* numEventos){
+    if(*numEventos == 0){
+        printf("Nenhum evento cadastrado.\n");
+        return;
+    }
     printf("Eventos cadastrados:\n");
     int i;
     for(i = 0; i < *numEventos; i++){
-        printf("%d - %s - %02d/%02d/%d\n", i, eventos[i].nome, eventos[i].dia, eventos[i].mes, eventos[i].ano);
+        printf("%d - %s - %d/%d/%d\n", i, eventos[i].nome, eventos[i].dia, eventos[i].mes, eventos[i].ano);
     }
 }
 
 void trocarDataEvento(Evento* eventos, int* numEventos){
     int n;
     scanf("%d ", &n);
+    if(n >= *numEventos){
+        printf("Indice invalido!\n");
+        return;
+    }
     scanf("%d %d %d ", &eventos[n].dia, &eventos[n].mes, &eventos[n].ano);
+    printf("Data modificada com sucesso!\n");
 }
 
 void trocarIndicesEventos(Evento* eventos, int* indiceA, int* indiceB, int* numEventos){
+    if(*indiceA >= *numEventos || *indiceB >= *numEventos){
+        printf("Indices invalidos!\n");
+        return;
+    }
     Evento temp;
     temp = eventos[*indiceA];
     eventos[*indiceA] = eventos[*indiceB];
     eventos[*indiceB] = temp;
+    printf("Eventos trocados com sucesso!\n");
 }
